ea1f07189165bce2865b0780550c619b03d19859
mapserver2007 <mapserver2007@gmail.com>
2013-09-08 22:47:48 +0900

    メソッド追加

--- core/WebStream/Test/TestConstant.php
+++ core/WebStream/Test/TestConstant.php
@@ -7,4 +7,29 @@ trait TestConstant
     {
         return "http://localhost/WebStream/core/test/testdata";
     }
+
+    private function getProjectRootPath()
+    {
+        return "/Users/stay/workspace2/WebStream";
+    }
+
+    private function getSampleAppPath()
+    {
+        return "/core/WebStream/Test/Sample";
+    }
+
+    private function getLogFilePath()
+    {
+        return $this->getSampleAppPath() . "/log/webstream.test.log";
+    }
+
+    private function getLogConfigPath()
+    {
+        return $this->getSampleAppPath() . "/config/log_config";
+    }
+
+    private function getCacheDir777()
+    {
+        return $this->getSampleAppPath() . "/cache777";
+    }
 }
