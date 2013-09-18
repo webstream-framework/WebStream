f862798236ec42df588c6660cf70c82f5bb796c6
mapserver2007 <mapserver2007@gmail.com>
2013-09-18 11:43:12 +0900

    LoggerExceptionを追加

---
+++ core/WebStream/Exception/LoggerException.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebStream\Exception;
+
+/**
+ * LoggerException
+ * @author Ryuichi TANAKA.
+ * @since 2013/09/08
+ * @version 0.4
+ */
+class LoggerException extends \Exception
+{
+}
