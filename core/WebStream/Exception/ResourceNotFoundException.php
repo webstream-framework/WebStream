8a05f1fcf47754a92820ad36e0933518f09df4f0
mapserver2007 <mapserver2007@gmail.com>
2013-09-21 00:54:03 +0900

    例外クラス追加

---
+++ core/WebStream/Exception/ResourceNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebStream\Exception;
+
+/**
+ * ResourceNotFoundException
+ * @author Ryuichi TANAKA.
+ * @since 2013/09/18
+ * @version 0.4
+ */
+class ResourceNotFoundException extends \Exception
+{
+}
---
+++ core/WebStream/Exception/RouterException.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebStream\Exception;
+
+/**
+ * RouterException
+ * @author Ryuichi TANAKA.
+ * @since 2013/09/08
+ * @version 0.4
+ */
+class RouterException extends \Exception
+{
+}
