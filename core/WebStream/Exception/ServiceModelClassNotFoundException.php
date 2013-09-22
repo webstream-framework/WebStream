08503ab2479b0a273d74eb247f5ab78a3ae2d1d1
mapserver2007 <mapserver2007@gmail.com>
2013-09-22 18:23:50 +0900

    例外クラス追加

---
+++ core/WebStream/Exception/ServiceModelClassNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebStream\Exception;
+
+/**
+ * ServiceModelClassNotFoundException
+ * @author Ryuichi TANAKA.
+ * @since 2013/09/22
+ * @version 0.4
+ */
+class ServiceModelClassNotFoundException extends \Exception
+{
+}
