ef6b0212944d524eab83726219b62c797a66a5b8
mapserver2007 <mapserver2007@gmail.com>
2013-09-22 11:41:22 +0900

    例外クラスを追加

---
+++ core/WebStream/Exception/ClassNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebStream\Exception;
+
+/**
+ * ClassNotFoundException
+ * @author Ryuichi TANAKA.
+ * @since 2013/09/22
+ * @version 0.4
+ */
+class ClassNotFoundException extends \Exception
+{
+}
---
+++ core/WebStream/Exception/MethodNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebStream\Exception;
+
+/**
+ * MethodNotFoundException
+ * @author Ryuichi TANAKA.
+ * @since 2013/09/22
+ * @version 0.4
+ */
+class MethodNotFoundException extends \Exception
+{
+}
