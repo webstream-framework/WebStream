9e15fc61b5c4d55b57473fbe0f9c4b2f3078f417
mapserver2007 <mapserver2007@gmail.com>
2013-10-19 22:42:02 +0900

    不要なクラスを削除

--- core/test/testdata/app/controllers/TestNoServiceMethodController.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace WebStream;
-class TestNoServiceMethodController extends CoreController {
-    public function execute() {
-        echo $this->TestNoServiceMethod->get();
-    }
-}
--- core/test/testdata/app/models/TestNoServiceClassModel.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace WebStream;
-class TestNoServiceClassModel extends CoreModel {
-    public function get() {
-        return "no service class";
-    }
-}
