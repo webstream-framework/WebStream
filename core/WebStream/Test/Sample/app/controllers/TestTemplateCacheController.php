b735bc825ec222659bd1d611523a470d0ceeb3db
mapserver2007 <mapserver2007@gmail.com>
2013-11-10 16:48:29 +0900

    @TemplateCacheテストクラス追加

---
+++ core/WebStream/Test/Sample/app/controllers/TestTemplateCacheController.php
@@ -0,0 +1,20 @@
+<?php
+namespace WebStream\Test\TestData\Sample\App\Controller;
+
+use WebStream\Core\CoreController;
+use WebStream\Annotation\Inject;
+use WebStream\Annotation\Template;
+use WebStream\Annotation\TemplateCache;
+
+class TestTemplateCacheController extends CoreController
+{
+    /**
+     * @Inject
+     * @TemplateCache(expire=10)
+     * @Template("index.tmpl")
+     */
+    public function index1()
+    {
+        echo "test";
+    }
+}
