f53446ee9337b1f68d36ca6e4ff5a1fa5092926e
mapserver2007 <mapserver2007@gmail.com>
2013-11-15 23:42:31 +0900

    ハンドリング周りを修正

--- core/WebStream/Core/Application.php
+++ core/WebStream/Core/Application.php
@@ -2,10 +2,11 @@
 namespace WebStream\Core;
 
 use WebStream\Module\Container;
-use WebStream\Module\Utility;
 use WebStream\Delegate\Resolver;
 use WebStream\Module\Logger;
+use WebStream\Module\Utility;
 
+use WebStream\Exception\ApplicationException;
 use WebStream\Exception\RouterException;
 use WebStream\Exception\ResourceNotFoundException;
 use WebStream\Exception\ClassNotFoundException;
@@ -26,8 +27,14 @@ class Application
 
     /** アプリケーションファイルディレクトリ名 */
     private $app_dir = "app";
+    /** DocumentRoot */
+    private $documentRoot;
+    /** ApplicationRoot */
+    private $applicationRoot;
     /** Request */
     private $request;
+    /** Response */
+    private $response;
     /** Resolver */
     private $resolver;
     /** Container */
@@ -41,6 +48,7 @@ class Application
     {
         Logger::debug("Application start");
         $this->container = $container;
+        $this->response  = $container->response;
         $this->request   = $container->request;
     }
 
@@ -64,18 +72,47 @@ class Application
         /** アプリケーションディレクトリ */
         define('STREAM_APP_DIR', $this->app_dir);
         /** ドキュメントルートからプロジェクトディレクトリへのパスを定義 */
-        define('STREAM_BASE_URI', $this->request->getBaseURL());
-        define('STREAM_ROUTING_PATH', $this->request->getPathInfo());
-        define('STREAM_QUERY_STRING', $this->request->getQueryString());
+        // TODO ↓クエリストリングとか普通に必要だと思うので確認してから不要なら消す
+        // define('STREAM_BASE_URI', $this->request->getBaseURL());
+        // define('STREAM_ROUTING_PATH', $this->request->getPathInfo());
+        // define('STREAM_QUERY_STRING', $this->request->getQueryString());
         /** publicディレクトリ */
+
         define('STREAM_VIEW_SHARED', "_shared");
         define('STREAM_VIEW_PUBLIC', "_public");
         define('STREAM_VIEW_CACHE', "_cache");
         /** キャッシュprefix */
         define('STREAM_CACHE_PREFIX', "webstream-cache-");
-        /** レスポンスキャッシュID */
-        define('STREAM_RESPONSE_CACHE_ID',
-               md5(STREAM_BASE_URI . STREAM_ROUTING_PATH . STREAM_QUERY_STRING));
+
+        
+
+
+        // /** レスポンスキャッシュID */
+        // define('STREAM_RESPONSE_CACHE_ID',
+        //        md5(STREAM_BASE_URI . STREAM_ROUTING_PATH . STREAM_QUERY_STRING));
+    }
+
+    /**
+     * ドキュメントルートパスを設定する
+     * @param string ドキュメントルートパス
+     */
+    public function documentRoot($path)
+    {
+        $this->request->setDocumentRoot($path);
+        // アプリケーション設定のチェック
+        // $publicDir = $this->getRoot() . "/" . $this->app_dir . "/views";
+
+        // $regexp = preg_replace("/\//", "\\\/", $path);
+        // if (preg_match("/^" . $regexp . "(.+)/", $this->request->server("REQUEST_URI"), $matches)) {
+        //     $publicDir .= $matches[1];
+        // }
+
+
+        // Logger::debug($publicDir);
+        // Logger::debug($this->request->getPathInfo());
+
+        //Logger::debug($this->request->getBaseURL());
+        //Logger::debug("approot:" . $this->applicationRoot);
     }
 
     /**
@@ -83,91 +120,77 @@ class Application
      */
     public function run()
     {
-        $this->init();
-        $this->resolver = new Resolver($this->container);
         try {
-            // MVCレイヤへのリクエストの振り分けを実行する
-            $this->resolver->run();
+            $this->init();
+            $this->resolver = new Resolver($this->container);
+            $this->resolver->run(); // MVCレイヤへのリクエストの振り分けを実行する
+        } catch (ApplicationException $e) {
+            // アプリケーション内部エラーの場合は500
+            Logger::error($e->getMessage(), $e->getTraceAsString());
+            $this->response->move(500);
         } catch (CsrfException $e) {
             // CSRFエラーの場合は400
             Logger::error($e->getMessage(), $e->getTraceAsString());
             if (!$this->handle($e)) {
-                $this->move(400);
+                $this->response->move(400);
             }
         } catch (SessionTimeoutException $e) {
             // セッションタイムアウトの場合は404
             Logger::error($e->getMessage(), $e->getTraceAsString());
             if (!$this->handle($e)) {
-                $this->move(404);
+                $this->response->move(404);
             }
         } catch (ClassNotFoundException $e) {
             // 存在しないクラスアクセスの場合は500
             Logger::error($e->getMessage(), $e->getTraceAsString());
-            if (!$this->handle($e)) {
-                $this->move(500);
-            }
+            $this->response->move(500);
         } catch (MethodNotFoundException $e) {
-            // 存在しないクラスアクセスの場合は500
+            // 存在しないメソッドアクセスの場合は500
             Logger::error($e->getMessage(), $e->getTraceAsString());
-            if (!$this->handle($e)) {
-                $this->move(500);
-            }
+            $this->response->move(500);
         } catch (InvalidRequestException $e) {
             // 許可されないメソッドの場合は405
             Logger::error($e->getMessage(), $e->getTraceAsString());
             if (!$this->handle($e)) {
-                $this->move(405);
+                $this->response->move(405);
             }
         } catch (ForbiddenAccessException $e) {
             // アクセス禁止の場合は403
             Logger::error($e->getMessage(), $e->getTraceAsString());
             if (!$this->handle($e)) {
-                $this->move(403);
+                $this->response->move(403);
             }
         } catch (ResourceNotFoundException $e) {
             // リソース(URI)が見つからない場合は404
             Logger::error($e->getMessage(), $e->getTraceAsString());
             if (!$this->handle($e)) {
-                $this->move(404);
+                $this->response->move(404);
             }
         } catch (AnnotationException $e) {
             // アノテーションエラーの場合は500
             Logger::error($e->getMessage(), $e->getTraceAsString());
-            if (!$this->handle($e)) {
-                $this->move(500);
-            }
+            $this->response->move(500);
         } catch (ValidateException $e) {
             // バリデーションエラーの場合は422
             Logger::error($e->getMessage(), $e->getTraceAsString());
             if (!$this->handle($e, $this->resolver->getValidateErrors())) {
-                $this->move(422);
+                $this->response->move(422);
             }
         } catch (RouterException $e) {
             // ルーティング解決失敗の場合は500
             Logger::error($e->getMessage(), $e->getTraceAsString());
-            if (!$this->handle($e, $this->resolver->getValidateErrors())) {
-                $this->move(500);
-            }
+            $this->response->move(500);
         }
     }
 
     /**
      * エラー処理のハンドリングチェック
-     * @param Object エラーオブジェクト
-     * @param Array エラー内容
-     * @return Boolean ハンドリングするかどうか
+     * @param object エラーオブジェクト
+     * @param array エラー内容
+     * @return boolean ハンドリングするかどうか
      */
     private function handle($errorObj, $errorParams = [])
     {
         return $this->resolver->handle($errorObj, $errorParams);
-    }
-
-    /**
-     * ステータスコードに合わせた画面に遷移する
-     * @param String ステータスコード
-     */
-    private function move($statusCode)
-    {
-        $this->resolver->move($statusCode);
     }
 }
