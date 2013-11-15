f3e73fd0bb19b7319408e0fe5c65770fada71afa
mapserver2007 <mapserver2007@gmail.com>
2013-11-15 23:45:50 +0900

    Application内部例外クラスを追加

---
+++ core/WebStream/Exception/ApplicationException.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebStream\Exception;
+
+/**
+ * ApplicationException
+ * @author Ryuichi TANAKA.
+ * @since 2013/11/12
+ * @version 0.4
+ */
+class ApplicationException extends \Exception
+{
+}
