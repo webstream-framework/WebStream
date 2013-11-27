a366e69b8623f2050da81c21f1c48164b9d46e68
mapserver2007 <mapserver2007@gmail.com>
2013-11-27 22:28:56 +0900

    例外クラス追加

---
+++ core/WebStream/Exception/ForbiddenAccessException.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebStream\Exception;
+
+/**
+ * ForbiddenAccessException
+ * @author Ryuichi TANAKA.
+ * @since 2013/11/26
+ * @version 0.4
+ */
+class ForbiddenAccessException extends \Exception
+{
+}
---
+++ core/WebStream/Exception/SessionTimeoutException.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebStream\Exception;
+
+/**
+ * SessionTimeoutException
+ * @author Ryuichi TANAKA.
+ * @since 2013/11/26
+ * @version 0.4
+ */
+class SessionTimeoutException extends \Exception
+{
+}
