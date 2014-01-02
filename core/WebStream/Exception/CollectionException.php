37a86c3b1d9a9ca2049a67e91ceaa9d2e583e2e7
mapserver2007 <mapserver2007@gmail.com>
2014-01-02 21:18:32 +0900

    例外クラスを追加

---
+++ core/WebStream/Exception/CollectionException.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebStream\Exception;
+
+/**
+ * CollectionException
+ * @author Ryuichi TANAKA.
+ * @since 2013/12/14
+ * @version 0.4
+ */
+class CollectionException extends \RuntimeException
+{
+}
---
+++ core/WebStream/Exception/DatabaseException.php
@@ -0,0 +1,12 @@
+<?php
+namespace WebStream\Exception;
+
+/**
+ * DatabaseException
+ * @author Ryuichi TANAKA.
+ * @since 2013/12/10
+ * @version 0.4
+ */
+class DatabaseException extends \Exception
+{
+}
