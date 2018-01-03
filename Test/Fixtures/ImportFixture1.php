1cfb84e119537605601c2a6ed080db49bb5e3d62
mapserver2007 <mapserver2007@gmail.com>
2018-01-03 11:14:32 +0900

    Squashed 'src/WebStream/ClassLoader/' content from commit e01e90d
    git-subtree-dir: src/WebStream/ClassLoader
    git-subtree-split: e01e90dba6f52cecb4a4acfb02a68823a5614332

---
+++ Test/Fixtures/ImportFixture1.php
@@ -0,0 +1,10 @@
+<?php
+namespace WebStream\ClassLoader\Test\Fixtures;
+
+class ImportFixture1
+{
+    public function method()
+    {
+        return 1;
+    }
+}
---
+++ Test/Fixtures/ImportFixture4.php
@@ -0,0 +1,10 @@
+<?php
+namespace WebStream\ClassLoader\Test\Fixtures;
+
+class ImportFixture4
+{
+    public function method()
+    {
+        return 1;
+    }
+}
